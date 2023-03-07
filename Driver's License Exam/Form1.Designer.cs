48cd9fb8d77fe739f0aaaf91b29febf9a11e39fa
rluma <107735661+rluma@users.noreply.github.com>
2023-03-06 18:26:08 -0700

    Add files via upload

---
+++ Driver's License Exam/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Driver_s_License_Exam
+{
+    partial class driversLicenseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.resultsButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.incorrectQuestionsLabel = new System.Windows.Forms.Label();
+            this.lboxInc = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(47, 26);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.fileNameLabel.TabIndex = 0;
+            this.fileNameLabel.Text = "File Name:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(131, 26);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(167, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // resultsButton
+            // 
+            this.resultsButton.Location = new System.Drawing.Point(36, 74);
+            this.resultsButton.Name = "resultsButton";
+            this.resultsButton.Size = new System.Drawing.Size(101, 23);
+            this.resultsButton.TabIndex = 2;
+            this.resultsButton.Text = "Read Results";
+            this.resultsButton.UseVisualStyleBackColor = true;
+            this.resultsButton.Click += new System.EventHandler(this.resultsButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(155, 74);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(104, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Location = new System.Drawing.Point(47, 114);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(45, 13);
+            this.resultsLabel.TabIndex = 4;
+            this.resultsLabel.Text = "Results:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(50, 152);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(248, 23);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // incorrectQuestionsLabel
+            // 
+            this.incorrectQuestionsLabel.AutoSize = true;
+            this.incorrectQuestionsLabel.Location = new System.Drawing.Point(47, 192);
+            this.incorrectQuestionsLabel.Name = "incorrectQuestionsLabel";
+            this.incorrectQuestionsLabel.Size = new System.Drawing.Size(102, 13);
+            this.incorrectQuestionsLabel.TabIndex = 6;
+            this.incorrectQuestionsLabel.Text = "Incorrect Questions:";
+            // 
+            // lboxInc
+            // 
+            this.lboxInc.FormattingEnabled = true;
+            this.lboxInc.Location = new System.Drawing.Point(50, 225);
+            this.lboxInc.Name = "lboxInc";
+            this.lboxInc.Size = new System.Drawing.Size(248, 121);
+            this.lboxInc.TabIndex = 7;
+            // 
+            // driversLicenseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 367);
+            this.Controls.Add(this.lboxInc);
+            this.Controls.Add(this.incorrectQuestionsLabel);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultsButton);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.fileNameLabel);
+            this.Name = "driversLicenseForm";
+            this.Text = "Driver\'s License Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button resultsButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label incorrectQuestionsLabel;
+        private System.Windows.Forms.ListBox lboxInc;
+    }
+}
+
---
+++ Driver's License Exam/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace Driver_s_License_Exam
+{
+    public partial class driversLicenseForm : Form
+    {
+        public driversLicenseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void resultsButton_Click(object sender, EventArgs e)
+        {
+            // Array of correct answers
+            string [] answerArray = {"B", "D", "A", "A", 
+                "C", "A", "B", "A", "C", "D", "B", "C", 
+                "D", "A", "D", "C", "C", "B", "D", "A" };
+
+            // Array of student's answers
+            string[] studentAnsArray = new string[20];
+
+            // Create list for incorrect question numbers
+            List<string> incorrectList = new List<string>();
+
+            // Create variables to hold correct count and index
+            int count = 0, index = 0, qnumber = 0;
+
+            try
+            {
+
+                // Open file if it exists
+                string filename = txtFileName.Text;
+                StreamReader inputFile = File.OpenText(filename);
+                while (!inputFile.EndOfStream)
+                {
+
+                    // Read line and put it into student answer array
+                    studentAnsArray[index] = inputFile.ReadLine();
+
+                    // Compare student answer array to answer array
+                    if (studentAnsArray[index] == answerArray[index])
+                        count++;
+                    else
+                    {
+                        qnumber = index + 1;  //add 1 to index to get the question number since they start at 1 and not at 0
+                        incorrectList.Add(qnumber.ToString());
+                    }
+                    // Increment index by 1
+                    index++;
+                }// Close loop
+                // Close file
+                inputFile.Close();
+                // Calculate results
+                if (count >= 15)
+                {
+                    lblResult.Text = "You passed the test!";
+                }
+                else
+                {
+                    lblResult.Text = "You failed the test.";
+                }
+                // Display incorrect answer numbers
+                foreach (string str in incorrectList)
+                {
+                   lboxInc.Items.Add(str);
+                }
+            }
+
+            catch (Exception)
+            {
+                // Display error message
+                MessageBox.Show("File not found.");
+            }
+                }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            txtFileName.Text = "";
+            lblResult.Text = "";
+            lboxInc.Items.Clear();
+        }
+    }
+}
